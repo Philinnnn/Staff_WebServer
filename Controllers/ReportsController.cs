ac809cd0c5e6d39a6f1286e06866e9482c44a7eb
Armando <armanela@bk.ru>
2025-04-19 21:23:01 +0500

    Director 1.0

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Staff_WebServer.Data;
 
+namespace Staff_WebServer.Controllers;
+
 [Authorize(Roles = "Director")]
 public class ReportsController : Controller
 {
@@ -13,12 +15,100 @@ public class ReportsController : Controller
         _context = context;
     }
 
-    public IActionResult DependentAllowances(int departmentId = 1)
+    public IActionResult Reports()
     {
-        var result = _context.Employees
-            .FromSqlRaw("SELECT * FROM fn_Рассчитать_начисления_на_иждивенцев({0})", departmentId)
-            .ToList();
+        ViewBag.Departments = _context.Departments.OrderBy(d => d.Name).ToList();
+        return View();
+    }
 
+    public IActionResult PensionerReport()
+    {
+        var result = new List<dynamic>();
+
+        var connection = _context.Database.GetDbConnection();
+        connection.Open();
+
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = @"
+            SELECT 
+                табельный_номер,
+                ФИО,
+                пол,
+                дата_рождения,
+                дата_увольнения,
+                dbo.fn_ПолныхЛет(дата_рождения) AS возраст
+            FROM Работники
+            WHERE дата_увольнения IS NOT NULL 
+              AND YEAR(дата_увольнения) = YEAR(GETDATE())
+              AND (
+                  (пол = N'м' AND dbo.fn_ПолныхЛет(дата_рождения) >= 63)
+                  OR (пол = N'ж' AND dbo.fn_ПолныхЛет(дата_рождения) >= 58)
+              )";
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    result.Add(new
+                    {
+                        FullName = reader["ФИО"].ToString(),
+                        Gender = reader["пол"].ToString(),
+                        BirthDate = Convert.ToDateTime(reader["дата_рождения"]),
+                        DismissDate = Convert.ToDateTime(reader["дата_увольнения"]),
+                        Age = reader["возраст"] == DBNull.Value ? (int?)null : Convert.ToInt32(reader["возраст"])
+                    });
+                }
+            }
+        }
+
+        connection.Close();
+        return View(result);
+    }
+
+    public IActionResult DependentAllowances(int departmentId)
+    {
+        var result = new List<dynamic>();
+
+        var connection = _context.Database.GetDbConnection();
+        connection.Open();
+
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = """
+
+                                              SELECT 
+                                                  табельный_номер,
+                                                  ФИО,
+                                                  оклад,
+                                                  кол_во_иждивенцев,
+                                                  dbo.fn_Рассчитать_начисления_на_иждивенцев(табельный_номер) AS Начисления
+                                              FROM Работники
+                                              WHERE код_подразделения = @dep
+                                  """;
+
+            var param = command.CreateParameter();
+            param.ParameterName = "@dep";
+            param.Value = departmentId;
+            command.Parameters.Add(param);
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    result.Add(new
+                    {
+                        Id = reader["табельный_номер"],
+                        FullName = reader["ФИО"].ToString(),
+                        Salary = Convert.ToDecimal(reader["оклад"]),
+                        Dependents = Convert.ToInt32(reader["кол_во_иждивенцев"]),
+                        Total = Convert.ToInt32(reader["Начисления"])
+                    });
+                }
+            }
+        }
+
+        connection.Close();
         return View(result);
     }
 }
--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Staff_WebServer.Models;
 
 public class ApplicationUser : IdentityUser
 {
+    [Column("табельный_номер")]
     public int ТабельныйНомер { get; set; }
+
+    public Employee Employee { get; set; }
 }
