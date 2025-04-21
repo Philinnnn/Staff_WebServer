a3afe5e4bd5e987dde826cd7e968a3e8b17d724d
Armando <armanela@bk.ru>
2025-04-21 18:42:57 +0500

    Admin account control fix

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -4,19 +4,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Staff_WebServer.Data;
+using Staff_WebServer.Models;
 
 namespace Staff_WebServer.Controllers;
 
 [Authorize(Roles = "Admin")]
-public class AdminController : Controller
+public class AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    : Controller
 {
-    private readonly ApplicationDbContext _context;
-
-    public AdminController(ApplicationDbContext context)
-    {
-        _context = context;
-    }
-
     public IActionResult Index()
     {
         return View();
@@ -24,7 +19,7 @@ public class AdminController : Controller
     
     public IActionResult AccessLogs()
     {
-        var logs = _context.AccessLogs
+        var logs = context.AccessLogs
             .OrderByDescending(log => log.Timestamp)
             .Take(100)
             .ToList();
@@ -45,7 +40,7 @@ public class AdminController : Controller
 
         string sql = $@"BACKUP DATABASE [Staff] TO DISK = N'{fullPath}' WITH INIT, FORMAT";
 
-        using (var connection = new SqlConnection(_context.Database.GetConnectionString()))
+        using (var connection = new SqlConnection(context.Database.GetConnectionString()))
         {
             connection.Open();
             using (var command = new SqlCommand(sql, connection))
@@ -88,5 +83,70 @@ public class AdminController : Controller
         }
 
         return RedirectToAction("Backups");
+    }
+
+    public IActionResult ManageUsers()
+    {
+        var users = userManager.Users
+            .OrderBy(u => u.Email)
+            .ToList();
+
+        return View(users);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ToggleLock(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        if (user != null)
+        {
+            if (user.LockoutEnd.HasValue && user.LockoutEnd > DateTime.UtcNow)
+                await userManager.SetLockoutEndDateAsync(user, DateTime.UtcNow); // unlock
+            else
+                await userManager.SetLockoutEndDateAsync(user, DateTime.UtcNow.AddYears(100)); // lock
+        }
+
+        return RedirectToAction("ManageUsers");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ResetPassword(string userId)
+    {
+        var user = await userManager.FindByIdAsync(userId);
+        if (user != null)
+        {
+            var token = await userManager.GeneratePasswordResetTokenAsync(user);
+            var newPass = GenerateSecurePassword();
+            var result = await userManager.ResetPasswordAsync(user, token, newPass);
+
+            TempData["Message"] = result.Succeeded
+                ? $"Пароль для {user.Email} сброшен. Новый пароль: {newPass}"
+                : $"❌ Ошибка: {string.Join(", ", result.Errors.Select(e => e.Description))}";
+        }
+
+        return RedirectToAction("ManageUsers");
+    }
+    
+    private static string GenerateSecurePassword()
+    {
+        const string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string lower = "abcdefghijklmnopqrstuvwxyz";
+        const string digits = "0123456789";
+        const string special = "!@#$%^&*";
+
+        var rnd = new Random();
+        return new string(Enumerable.Range(0, 8)
+            .Select(i =>
+                i switch
+                {
+                    0 => upper[rnd.Next(upper.Length)],
+                    1 => lower[rnd.Next(lower.Length)],
+                    2 => digits[rnd.Next(digits.Length)],
+                    3 => special[rnd.Next(special.Length)],
+                    _ => (upper + lower + digits + special)[rnd.Next(upper.Length + lower.Length + digits.Length + special.Length)]
+                }
+            )
+            .OrderBy(_ => rnd.Next())
+            .ToArray());
     }
 }
